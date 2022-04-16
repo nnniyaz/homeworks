c45b36bbc4112b6a48511f45b3716a4acc6907e4
👉Your name👈 <Your email>
2022-04-16 08:08:26 +0600

    Finished promise assignment

--- promises/task.js
+++ promises/task.js
@@ -1,2 +1,3 @@
-const result = promiseFunction()
-console.log(`Промис ${typeof result === 'string' ? '' : 'НЕ'} работает ${typeof result === 'string' ? result : ''}`)
+const result = promiseFunction().then((result)=>{
+    console.log(`Промис ${typeof result === 'string' ? '' : 'НЕ'} работает ${typeof result === 'string' ? result : ''}!`)
+});
