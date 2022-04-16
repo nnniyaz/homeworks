bd31ba4c932f31f61af2353372dfb04fd4afd3ab
👉Your name👈 <Your email>
2022-04-16 07:50:34 +0600

    Finished Arrays assignment

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,13 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  result = true;
+  if (arr1.length == arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) result = false;
+    }
+  }
+  else result = false;
 
   return result; // boolean
 }
@@ -10,6 +17,10 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  resultArr = arr.filter(function(item){
+    return item > 0 && item % 3 == 0;
+  });
+  resultArr = resultArr.map(item => item * 10);
 
   return resultArr; // array
 }
